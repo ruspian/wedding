d5333110c08ab7fe01e643a0bca689557d452346
ruspian <ruspianntb@gmail.com>
2025-05-09 23:27:55 +0800

    tambah coundown

--- countdown/index.js
+++ countdown/index.js
@@ -1,16 +1,13 @@
-
-
-simplyCountdown('.simply-countdown', {
-    year: 2024, // required
-    month: 5, // required
-    day: 12, // required
-    hours: 8, // Default is 0 [0-23] integer
-    words: { //words displayed into the countdown
-        days: { singular: 'Hari', plural: 'Hari' },
-        hours: { singular: 'Jam', plural: 'Jam' },
-        minutes: { singular: 'Menit', plural: 'Menit' },
-        seconds: { singular: 'Detik', plural: 'Detik' }
-    },
+simplyCountdown(".simply-countdown", {
+  year: 2025, // required
+  month: 6, // required
+  day: 7, // required
+  hours: 10, // Default is 0 [0-23] integer
+  words: {
+    //words displayed into the countdown
+    days: { singular: "Hari", plural: "Hari" },
+    hours: { singular: "Jam", plural: "Jam" },
+    minutes: { singular: "Menit", plural: "Menit" },
+    seconds: { singular: "Detik", plural: "Detik" },
+  },
 });
-
-
